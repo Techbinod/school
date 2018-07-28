bf9afe8a433cb2b94bf1fe546d7b75eec57e8482
Techbinod <35165977+Techbinod@users.noreply.github.com>
2018-07-28 09:57:56 +0545

    index_page_created

---
+++ index.php
@@ -0,0 +1,303 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
+    <script src="js/jquery2.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <title>TASS</title>
+    <style >
+    	@media screen and (max-width : 500px) {
+        div.photo {
+          display: none;
+        }
+         
+        }
+
+        @media screen and (max-width : 500px) {
+        div.carousel-caption {
+          display: none;
+        }
+        
+        }
+         
+        
+        .navbar{
+        	
+        	padding-top:0px;
+        	padding-bottom: 0px;
+        	margin-bottom: 0px;
+        	font-size: 14px;
+        	font-family: 'PT Serif', serif;
+
+        }
+
+        .nav-item:hover > .nav-link{
+        	color:yellow !important;
+        }
+
+        .dropdown:hover > .dropdown-menu{
+        	display:block;
+        	margin:0px;
+        	padding:0px;
+        	
+        }
+
+        .dropdown-item:hover {
+        	background-color:yellow !important;
+        	margin:0px;
+
+        }
+
+        .social_media{
+        	font-size: 20px;
+        	/* smargin-right: 15px; */
+        	display:inline-block;
+        
+        } 
+          .social_media > li {
+        	
+        	display:inline-block;
+        	margin-left: 20px;
+        } 
+        .phone > a{
+       	 margin-left: 20px;
+         text-decoration: none;
+         color:white;
+        
+        }
+      
+         .carousel-caption {
+		    max-width: 100%;
+		    width:100%;
+		    background-color: rgba(0,0,0,0.5);
+		    left: 0;
+		    bottom: 0px;
+		    padding-bottom: 0px;
+        }
+        .notice_caption{
+        	background-color: red;
+
+        }
+        .row_first{
+        	background-color: red;
+
+        	/* border-width: 2px solid black; */
+        }
+        .row{
+        	margin-right: 0px;
+        	margin-left: 0px;
+        	margin-top: 0px;
+        	/* background-color: blue; */
+        }
+        
+        
+        .card-header{
+        	background-color:#B61515;
+        	color:#fcfcfc;
+        	padding:5px;
+        	margin-top: 2px;
+        	
+        }
+
+
+        .card  p {
+        	text-align:justify;
+        	display: list-item;
+            line-height: 17px;
+
+        }
+
+        .card a{
+        	
+        	text-decoration: none;
+            color:#000;
+        }
+
+        .card a:hover{
+        	
+        	text-decoration: none;
+            color:red;
+        }
+      
+
+          
+         
+    </style>
+    
+  </head>
+  <body>
+
+
+    <div class="contianer-fluid photo">
+        <img style="width:100%;height:100px; background-color:#194576;" src="photoes/taass.png" alt="last" class="img-responsive">  
+    </div>
+
+
+          <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+
+          	  <div class="container-fluid">
+    			  
+
+    			  <button  class="navbar-toggler"  data-toggle="collapse" data-target="#navbarSupportedContent">
+    			    
+    			    	<span  class="navbar-toggler-icon" ></span>
+    			    	<span style="color:#000 !important">Menu</span>
+    			            
+    			  </button>
+    
+    			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    			    <ul class="navbar-nav text-cente mr-auto" >
+	    			      <li class="nav-item active">
+	    			        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	    			      </li>
+	    			      <li class="nav-item">
+	    			        <a class="nav-link" href="#">About Us</a>
+	    			      </li>
+	    
+	    			      <li class="nav-item dropdown">
+	    			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	    			          Academic Programs
+	    			        </a>
+
+	    			        <div class="dropdown-menu">
+	    			          <a class="dropdown-item " href="#">Action</a>
+	    			          <a class="dropdown-item " href="#">Another action</a>
+	    			      </div>
+	    			          
+	    			      </li>
+	    			      <li class="nav-item">
+	    			        <a class="nav-link" href="#">Admission</a>
+	    			      </li>
+	    			      <li class="nav-item ">
+	    			        <a class="nav-link" href="#">Facilities</a>
+	    			      </li>
+	    			      <li class="nav-item  ">
+	    			        <a class="nav-link" href="#">Photo Gallery</a>
+	    			      </li>
+	    			      <li class="nav-item dropdown">
+	    			        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	    			          Students
+	    			        </a>
+
+	    			        <div class="dropdown-menu">
+	    			          <a class="dropdown-item " href="#">Present</a>
+	    			          <a class="dropdown-item " href="#">Alumni</a>
+	    			      </div>
+	    			          
+	    			      </li> 
+	    			      <li class="nav-item  ">
+	    			        <a class="nav-link" href="#">Contact Us</a>
+	    			      </li>  
+    			       
+    			    </ul>
+    			    
+    			    <ul class="navbar-nav ml-auto social_media ">
+    			    	
+    			        <li class="nav-item  ">
+	    			       <a style =" color:white; " href="#"><i class="fab fa-facebook-f"></i></a>
+	    			    </li>
+	    			    
+	    			    <li class="nav-item ">
+	    			       <a style =" color:#FF0000; "  href="#"><i class="fab fa-youtube"></i></a>
+	    			    </li>
+	    			    <li class="nav-item  ">
+	    			        <a  style = "color:#5DCCF1";href="#"><i class="fab fa-twitter"></i></a>
+	    			    </li> 
+	    			     
+	    			     
+	    			    <li class="nav-item phone ">
+	    			    	<a href="tel:+6494461709"><i class="fas fa-phone-volume"></i>  9861866606</a>
+	    			    </li>
+	    			     
+    			    </ul>
+
+    			   
+    			    
+    			  </div>
+    	   </div>
+     </nav>   
+
+
+<div class="contianer-fluid ">
+	<div class="row" >
+		<div class="col-md-9">
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+			  <div class="carousel-inner" >
+			    <div class="carousel-item active">
+			      <img class="d-block w-100" src="photoes/group.jpg" alt="First slide">
+			      <div class="carousel-caption">
+				     <p>Participation on welcome program</p>   
+				   </div>
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="photoes/group.jpg" alt="Second slide">
+			      <div class="carousel-caption">
+				     <p>Participation on welcome program</p>   
+				   </div>
+
+			    </div>
+			    <div class="carousel-item">
+			      <img class="d-block w-100" src="photoes/group.jpg" alt="Third slide">
+			      <div class="carousel-caption">
+				     <p>Participation on welcome program</p>   
+				   </div>
+			    </div>
+			  </div>
+			  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			  </a>
+			</div>
+		</div> 
+
+
+		<div class="col-md-3 text-center ">
+		     <div class="card" style="width:100%">
+				  <div class="card-header ">
+				     <h4>News & Events</h4>
+				  </div>
+				  <ul class="list-group list-group-flush ">
+				    <li class="list-group-item ">
+				    	
+				    	<a  href="#">
+                              <h5>March 3,2018</h5>
+				    	      <p>Class will remain close today and will resume from sunday</p>
+                         </a>
+                         
+				    </li>
+				    <li class="list-group-item ">
+				    	<h5>March 3,2018</h5>
+				    	<p>Class will remain close today and will resume from sunday</p>
+				    </li>
+				    <li class="list-group-item ">
+				    	<h5>March 3,2018</h5>
+				    	<p>Class will remain close today and will resume from sunday</p>
+				    </li>
+				  </ul>
+
+            </div>
+	
+		</div>
+		
+</div>
+
+    
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
